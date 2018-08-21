1b7f7ebf7eb2a10cb0fa97f6d3d483b74aff9578
xiaoxiyouran <xiaoxiyouran@163.com>
2018-08-21 22:59:14 +0800

    update

---
+++ python2/201801821python3_7liaoxuefeng/unmd/tmp_pycharm_project/01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+filename: 01.py
+message: notes for liaoxuefeng in learning pyhon 3.7
+'''
+
+name1=input('pleas enter first num:')
+name2=input('please enter  second num:')
+print(int(name1))
+print(int(name2))
+print('%s * %s=' % (name1,name2), int(name1) * int(name2))
+
+# pleas enter first num:1
+# please enter  second num:2
+# 1
+# 2
+# 1 * 2= 2
---
+++ python2/201801821python3_7liaoxuefeng/unmd/tmp_pycharm_project/02.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+filename: 02.py
+message: notes for liaoxuefeng in learning pyhon 3.7
+'''
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+print('n = ', n)
+print('f = ', f)
+print('s1 = ', s1)
+print('s2 = ', s2)
+print('s3 = ', s3)
+print('s4 = ', s4)
+
+# n =  123
+# f =  456.789
+# s1 =  Hello, world
+# s2 =  Hello, 'Adam'
+# s3 =  Hello, "Bart"
+# s4 =  Hello,
+# Lisa!
